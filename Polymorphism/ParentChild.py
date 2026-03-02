20dae483039fef2561af091e31de8684561b557f
Kiran Takri <kirantakri18@gmail.com>
2026-03-02 09:33:58 +0530

    Updated Code

--- Polymorphism/ParentChild.py
+++ Polymorphism/ParentChild.py
@@ -2,11 +2,19 @@ class Parent:
     def show(self):
         print("Parent name is Parent")
         print("Parent age is 50")
-class Child(Parent):
+class Child1(Parent):
     
     def show(self):
         super().show()
-        print("Child name is Child")
+        print("Child name is Child1")
         print("Child age is 20")
-c=Child()
+
+class Child2(Parent):
+    def show(self):
+        super().show()
+        print("Child name is Child2")
+        print("Child age is 24")
+c=Child1()
+c.show()
+c=Child2()
 c.show()
